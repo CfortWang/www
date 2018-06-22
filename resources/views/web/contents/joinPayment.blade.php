af17e2bdd9fe674ff390468098464e9980740db2
wood <jiumutou@126.com>
2018-06-22 16:23:38 +0800

    wood

--- resources/views/web/contents/joinPayment.blade.php
+++ resources/views/web/contents/joinPayment.blade.php
@@ -15,20 +15,17 @@
             <div class="col-sm-6"><span>用户名：</span>{{Session::get('name')}}<span></span></div>
         </div>
         <div class="joinPayment-content">
-            <ul class="nav nav-tabs">
+            <ul class="nav nav-tabs" style="border-bottom:none">
                 <li class="joinPayment-li active"><a href="javascript:;" id="online-contain">在线支付</a></li>
                 <li class="joinPayment-li"><a href="javascript:;" id="tran-contain">银行汇款</a></li>
             </ul>
             <div class="pay-contain online-contain">
-                <p>转账汇款，请先联系在线客服。</p>
-
-                <p>1.由于转账汇款时间较长，将为您带来诸多不便，请您谅解。</p>
-
-                <p>2.在完成转账或付款业务后，请您尽快与我们联系（400-XXX-XXX 或 在线客服），并提供姓名、金额、用户名。</p>
-                <p>单位名称：深圳喜豆文化发展有限公司</p>
-
-                <p>开户银行：招商银行股份有限公司深圳南硅谷支行</p>
-
+                <div>
+                    <a >支付宝支付</a>
+                </div>
+                <div>
+                    <a>银联支付</a>
+                </div>
             </div>
             <div class="pay-contain tran-contain" style="display:none">
                 <p>转账汇款，请先联系在线客服。</p>
