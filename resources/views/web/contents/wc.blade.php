b54de8181b06ba5f63db0a11f98fcbebf2baaf50
wood <jiumutou@126.com>
2018-07-02 11:42:53 +0800

    w

--- resources/views/web/contents/wc.blade.php
+++ resources/views/web/contents/wc.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>竞猜</title>
+    <title>世界杯竞猜</title>
     <link rel="icon" href="/img/seedo.ico" type="image/x-icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
