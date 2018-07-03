72f55818fa7cbca9370f9a1b90050faf3ad65bdb
wood <jiumutou@126.com>
2018-07-03 14:39:02 +0800

    wood

--- resources/views/web/contents/wc.blade.php
+++ resources/views/web/contents/wc.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" type="text/css" href="./css/bootstrapValidator.css" />
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="./js/bootstrapValidator.min.js"></script>
+    
 </head>
 
 <body>
@@ -230,6 +230,7 @@
         </div>
     </div>
 </body>
+<script src="./js/bootstrapValidator.min.js"></script>
 <script>
     $('#back').bootstrapValidator({
             message: 'This value is not valid',
