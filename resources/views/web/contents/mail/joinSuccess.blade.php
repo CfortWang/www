84b005e9fe44b62f4bd7db7c4d3bade4e18808ca
CfortWang <cfortwang@gmail.com>
2018-07-09 17:19:08 +0800

    cfort

--- resources/views/web/contents/mail/joinSuccess.blade.php
+++ resources/views/web/contents/mail/joinSuccess.blade.php
@@ -1,2 +1,59 @@
-<p>{{$name}}，这是一封来自喜豆的测试邮件</p>
-<img src="http://www.beanpop.cn/img/phone.png">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Page Title</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body style="margin: 0; padding: 0; font-size: 62.5%;">
+		<table border="0" cellspacing="0" cellpadding="0" style="background-image: url(/img/mail/background.png); background-size: 100% 100%; width: 100%; text-align: center; font-size: 1.4rem; font-weight: bold;">
+			<tr>
+				<th style="text-align: right; padding-right: 8rem; padding-top: 3rem; height: 4rem;">授权书合同号：BP201807004001</th>
+			</tr>
+			<tr>
+				<td><img style="width: 8rem; border:none; outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;" src="/img/mail/logo.png" /></td>
+			</tr>
+			<tr>
+				<td style="font-size: 3rem; color: #4b6db4; letter-spacing: 1rem; padding-top: 1rem; padding-left: 1rem;">授权证书</td>
+			</tr>
+			<tr>
+				<td style="font-size: 1rem; color: #284ea1;">CERTIFICATE OF AUTHORIZATION</td>
+			</tr>
+			<tr>
+				<td style="padding-top: 1.5rem;">
+					<div style="font-size: 1.6rem;">王松</div>
+					<hr style="width: 65%; size: 1px; border: 0.5px solid #4b6db4;;" />
+				</td>
+			</tr>
+			<tr>
+				<td style="padding-top: 1rem;">兹授权<span style="margin-left: 1rem; margin-right: 1rem;">王松</span>为深圳喜豆文化发展有限公司<span style="margin-left: 1rem; margin-right: 1rem;">广东省深圳市南山区</span>销售合伙人，</td>
+			</tr>
+			<tr>
+				<td style="padding-top: 1rem;">获得我司在该授权区域所有产品的销售权限。</td>
+			</tr>
+			<tr>
+				<td style="padding-top: 2rem;">
+					授权期限为两年
+					<span style="margin-left: 1rem; margin-right: 1rem;">2018</span>年
+					<span style="margin-left: 1rem; margin-right: 1rem;">7</span>月
+					<span style="margin-left: 1rem; margin-right: 1rem;">1</span>日至
+					<span style="margin-left: 1rem; margin-right: 1rem;">2019</span>年
+					<span style="margin-left: 1rem; margin-right: 1rem;">7</span>月
+					<span style="margin-left: 1rem; margin-right: 1rem;">1</span>日为止。
+				</td>
+			</tr>
+			<tr>
+				<td  style="padding-top: 1rem; font-weight: 500; font-size: 1rem;">备注：本授权书以正本为有效文本，不得影印，涂改，转让</td>
+			</tr>
+			<tr>
+				<td style="padding-bottom: 4rem;">
+					<div style="position: relative; width: 12rem; height: 12rem; float: right; right: 17rem;"><img style="width: 100%; height: 100%;" src="/img/mail/stamp.png"/></div>
+				</td>
+			</tr>
+		</table>
+	</body>
+
+</html>
