10840012261780dbfbf1e5eb2a0dba8cb1bd952f
CfortWang <cfortwang@gmail.com>
2018-07-09 18:52:50 +0800

    cfort

--- resources/views/web/contents/mail/joinSuccess.blade.php
+++ resources/views/web/contents/mail/joinSuccess.blade.php
@@ -6,19 +6,12 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Page Title</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-        <style type="text/css">
-			@media all and (max-width: 780px) {
-				body{
-					color: red;
-				}
-			}
-		</style>
 	</head>
 
 	<body style="margin: 0; padding: 0; font-size: 62.5%;">
 		<table border="0" cellspacing="0" cellpadding="0" style="background-image: url(http://dev-www.beanpop.cn/img/mail/background.png); background-size: 100% 100%; width: 100%; text-align: center; font-size: 1.4rem; font-weight: bold;">
 			<tr>
-				<th style="text-align: right; padding-right: 8rem; padding-top: 3rem; height: 4rem;">授权书合同号：BP201807004001</th>
+				<th style="text-align: right; padding-right: 10%; padding-top: 3rem; height: 4rem;">授权书合同号：BP201807004001</th>
 			</tr>
 			<tr>
 				<td><img style="width: 8rem; border:none; outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;" src="http://dev-www.beanpop.cn/img/mail/logo.png" /></td>
@@ -36,28 +29,22 @@
 				</td>
 			</tr>
 			<tr>
-				<td style="padding-top: 1rem;">兹授权<span style="margin-left: 1rem; margin-right: 1rem;">王康辉</span>为深圳喜豆文化发展有限公司<span style="margin-left: 1rem; margin-right: 1rem;">广东省深圳市南山区</span>销售合伙人，</td>
+				<td style="padding-top: 1rem; padding-left: 20px; padding-right: 20px;">兹授权<span style="margin-left: 1rem; margin-right: 1rem;">王康辉</span>为深圳喜豆文化发展有限公司<span style="margin-left: 1rem; margin-right: 1rem;">广东省深圳市南山区</span>销售合伙人，</td>
 			</tr>
 			<tr>
-				<td style="padding-top: 1rem;">获得我司在该授权区域所有产品的销售权限。</td>
+				<td style="padding-top: 1rem; padding-left: 20px; padding-right: 20px;">获得我司在该授权区域所有产品的销售权限。</td>
 			</tr>
 			<tr>
-				<td style="padding-top: 2rem;">
-					授权期限为两年
-					<span style="margin-left: 1rem; margin-right: 1rem;">2018</span>年
-					<span style="margin-left: 1rem; margin-right: 1rem;">7</span>月
-					<span style="margin-left: 1rem; margin-right: 1rem;">1</span>日至
-					<span style="margin-left: 1rem; margin-right: 1rem;">2019</span>年
-					<span style="margin-left: 1rem; margin-right: 1rem;">7</span>月
-					<span style="margin-left: 1rem; margin-right: 1rem;">1</span>日为止。
+				<td style="padding-top: 2rem; padding-left: 20px; padding-right: 20px;">
+                    授权期限为两年&nbsp;<span >2018</span>&nbsp;年&nbsp;<span >7</span>&nbsp;月&nbsp;<span>1</span>&nbsp;日至&nbsp;<span >2019</span>&nbsp;年&nbsp;<span >7</span>&nbsp;月&nbsp;<span >1</span>&nbsp;日为止。
 				</td>
 			</tr>
 			<tr>
-				<td  style="padding-top: 1rem; font-weight: 500; font-size: 1rem;">备注：本授权书以正本为有效文本，不得影印，涂改，转让</td>
+				<td  style="padding-top: 1rem; padding-left: 20px; padding-right: 20px; font-weight: 500; font-size: 1rem;">备注：本授权书以正本为有效文本，不得影印，涂改，转让</td>
 			</tr>
 			<tr>
 				<td style="padding-bottom: 4rem;">
-					<div style="position: relative; width: 12rem; height: 12rem; float: right; right: 17rem;"><img style="width: 100%; height: 100%;" src="http://dev-www.beanpop.cn/img/mail/stamp.png"/></div>
+					<div style="position: relative; width: 10rem; height: 10rem; float: right; right: 10%;"><img style="width: 100%; height: 100%;" src="http://dev-www.beanpop.cn/img/mail/stamp.png"/></div>
 				</td>
 			</tr>
 		</table>
